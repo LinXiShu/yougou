1cd6e795c8c812e465cbc96025e12fffd7077183
linxishu <876797051@qq.com>
2018-06-13 15:56:25 +0800

    提交 2018/6/13

---
+++ admin/js/admin.js
@@ -0,0 +1,8 @@
+/* 
+* @Author: Marte
+* @Date:   2018-06-13 15:32:03
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-06-13 15:37:19
+*/
+
+
--- supermarket/api/server2.js
+++ supermarket/api/server2.js
@@ -19,103 +19,155 @@ app.use(function(req,res,next){
 
     next();
 
-})
+});
 
 app.get("/", (req, res) => {
 
     res.send("hello LinXiShu");
 
-})
+});
 
-app.post("/signins", (req, res) => {
-    
+function RndNum(n){
+    var rnd="";
+    for(var i=0;i<n;i++)
+        rnd+=Math.floor(Math.random()*10);
+
+        return rnd;
+}
+
+var id;
+
+// 会员 注册
+app.post("/signin", (req, res) => {
     res.append("Access-Control-Allow-Origin","*");
-
     var params = {
-        username: req.body.username,
-        password: req.body.password
+        phone : req.body.phone,
+        password : req.body.password
     }
 
-    console.log(params.username,123)
+    var signin_sql = db.query("select * from users where phone='"+params.phone+"'", null , function(ZC){
+        id = RndNum(5);
+        if(ZC == ''){
+            var signin_sql2 = db.query("insert into users( id, phone, password ) values("+ id + ",'"+ params.phone + "'," + params.password + ')', null , function(CR){
+                res.send('yesSignin');
+            });
+        };
+            if(ZC != ''){
+                console.log(123);
+                res.send('yesUser');
+            }
+    })
+});
 
-    var sql3 = db.body('select * FROM users where username ="' + params.username  + '"',null, function(a){
+// 会员 登录
+app.post("/login", (req, res) => {
 
-        console.log(a);
+    let params = {
+        phone : req.body.phone,
+        password : req.body.password
+    };
+    var login_sql = db.query("select * from users where phone='"+ params.phone +"' and password='"+ params.password +"'" , null, function(FH){
+        if(FH == ''){
+            console.log(FH);
+            res.send('failLogin');
+        };
+        if(FH != ''){
+            console.log(FH);
+            res.send(FH);
+        };
+    })
+});
 
-        res.send("帐号已经存在");
+// 获取 所有 商品
 
-    });
+app.get("/products", ( req, res) => {
 
+    var products_sql = db.query("select * from products ", null , function(SP){
 
+        console.log(SP);
 
-    var sql2 = db.body("insert into users(username, password) values('" + params.username + "'," + params.password + ')', function(results){
+        // let obj = new Object();
 
-         res.send("帐号注册成功");
+        // for(let x in SP){
+
+        //     obj[x] = SP[x]
+
+        // };
+
+        res.send(JSON.stringify(SP));
 
     })
 
 });
 
-// 会员 注册
-app.post("/signin", (req, res) => {
+// 购物车 获取数据      X
 
-    res.append("Access-Control-Allow-Origin","*");
+// app.get("/car", ( req, res) => {
+
+//     var car_select = db.query("select * from ");
+
+// })
+
+
+
+//  购物车 加入数据
+//  传 id  
+
+// 谢谢你 ，一直保佑我的人 。
+
+app.get("/car_cr", (req, res) => {
 
     var params = {
-        username : req.body.username,
-        password : req.body.password
-    }
 
-
-
-    var signin_sql = db.query("select * from users where username='"+params.username+"'", null , function(ZC){
-
-    if(ZC == ''){
-
-        res.send('noUser');
+        id : req.query.id,
+        ip : "'" + req.query.ip + "'"
     };
 
-    if(ZC != ''){
+    var car_sql3 = db.query( " select * from car where id = "+ params.id +" and ip = "+ params.ip  , null , function(car_CC){
 
-        
+        // if(car_CC == ''){
 
-        var signin_sql2 = db.query("", null , function(){
+        //     res.send('noCarProducts');
+        // }
+        // if(car_CC != ''){
 
+        //     res.send('yesCarProducts');
+        // }
 
-        })
+        console.log(car_CC);
 
-    }
+        res.send('yesCar');
 
+    });
 
-    })
+    //     var car_sql = db.query("select * from products where id="+ params.id, null , function(car_fh){
 
-})
+    //         if(car_fh == ''){
+    //             res.send('都没有这个商品，你是来搞笑的吗?');
+    //         };
+    //         if( car_fh != '' ){
+    //                    let id         = params.id;
+    //                    let name       = '"'+ car_fh[0].name +'"';
+    //                    let price      = '"'+ car_fh[0].price +'"';
+    //                    let qty        = car_fh[0].qty;
+    //                    let classes    = car_fh[0].classes;
+    //                    let photo      = '"'+ car_fh[0].photo +'"';
+    //                    let ip         = '"'+ params.ip +'"';
 
+    //                     console.log( id, name, photo , price , qty , classes , ip );
+    //                     var car_sql2 = db.query( "insert into car( id, name, photo , price , qty , classes , ip ) values("+id+","+name+","+photo+","+price+","+qty+","+classes+","+ip+")" , null , function(car_fh2){
+    //                         console.log(car_fh2,123);
+    //                         res.send('yesCar_cr');
 
-// 会员 登录
-app.post("/login", (req, res) => {
+    //                    });
 
-    res.append("Access-Control-Allow-Origin","*");
-
-    let params = {
-        username : req.body.username,
-        password : req.body.password
-    };
-
-    console.log(params);
-
-    var login_sql = db.query("select * from users where username='"+ params.username +"' and password='"+ params.password +"'" , null, function(FH){
-
-        if(FH === ''){
-            res.send('failLogin');
-        };
-        if(FH != ''){
-            res.send('successLogin');
-        }
-
-    })
+    //                     // res.send(JSON.stringify(car_fh));
+    //         };
+    // })
     
 })
+
+
 
 var server = app.listen(1898, function(){
 
@@ -126,5 +178,4 @@ var server = app.listen(1898, function(){
     var port = server.address().port;
 
 });
-
 
